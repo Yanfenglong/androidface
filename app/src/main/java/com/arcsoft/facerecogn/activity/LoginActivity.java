f7d1f7d98d29eb693ccf8a31b0b3b15e8243525c
严凤龙 <everisnever@163.com>
2020-07-26 17:10:03 +0800

    init

---
+++ app/src/main/java/com/arcsoft/facerecogn/activity/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.arcsoft.facerecogn.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.arcsoft.facerecogn.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
---
+++ app/src/main/java/com/arcsoft/facerecogn/common/Constants.java
@@ -0,0 +1,8 @@
+package com.arcsoft.facerecogn.common;
+
+    public class Constants {
+    //public static final String APP_ID = "APP ID:13wb23BR4UNpiNBciCJZhboRYgnfE7PPtfiJR9tRTpct";
+    public static final String APP_ID = "13wb23BR4UNpiNBciCJZhboRYgnfE7PPtfiJR9tRTpct";
+    //public static final String SDK_KEY = "SDK KEY:GgxhWwEpNq8ArG3GAongLzAur84Mz1jwBx6HNYxuHeeR";
+    public static final String SDK_KEY = "GgxhWwEpNq8ArG3GAongLzAur84Mz1jwBx6HNYxuHeeR";
+}
